be18c0603b50851b5d15bea73bf309ddb2f7b405
hortizrd <hectorortiz681@gmail.com>
2020-04-14 23:35:49 -0400

    He completado el proceso de crear una cuenta de usuario Cargar imagen en firebase

--- app/src/main/java/com/example/blogapp/Activities/HomeActivity.java
+++ app/src/main/java/com/example/blogapp/Activities/HomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.blogapp;
+package com.example.blogapp.Activities;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.blogapp.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+        setContentView( R.layout.activity_home );
     }
 }
