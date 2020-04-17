7fb8ad310a39b1e2ff4f5f76f4dd84bb3c2e72cf
hortizrd <hectorortiz681@gmail.com>
2020-04-17 00:21:58 -0400

    Se ha creado el acceso de login con un correo y su respectiva contraseña

--- app/src/main/java/com/example/blogapp/Activities/HomeActivity.java
+++ app/src/main/java/com/example/blogapp/Activities/HomeActivity.java
@@ -1,16 +1,44 @@
 package com.example.blogapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.blogapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private Button btnSalir;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_home );
+
+        btnSalir =findViewById( R.id.home_btnsalir );
+        mAuth = FirebaseAuth.getInstance();
+
+        btnSalir.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+                Salir();
+            }
+        } );
+
+    }
+
+    private void signOut(){
+        mAuth.signOut();
+        finish();
+    }
+    public void Salir(){
+
+        Intent RegresarLogin = new Intent( getApplicationContext(),LoginActivity.class );
+        startActivity( RegresarLogin );
     }
 }
